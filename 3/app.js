bd552df8e8ea49cfe2ae079259a4f65888a7fb10
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2022-09-13 17:06:40 +0100

    serve static file

--- 3/app.js
+++ 3/app.js
@@ -8,6 +8,7 @@ const userRouter = require('./routes/userRoutes');
 // 1)middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
 
     console.log('hello from the middleware!');
