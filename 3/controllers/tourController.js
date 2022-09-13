4f8cf9ee495c93c95eb12db613c288a7754400ec
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2022-09-13 17:00:36 +0100

    done with middleware setup

--- 3/controllers/tourController.js
+++ 3/controllers/tourController.js
@@ -2,7 +2,26 @@ const fs = require('fs');
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
-// 2)route handlers
+exports.checkID =  (req,res,next,val)=>{
+    console.log('Tour id is '+ val);
+    const id = req.params.id * 1;
+    if(id > tours.length){
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid id'
+        });
+    }
+    next();
+}
+exports.checkBody = (req,res,next)=>{
+    if(!req.body.name || !req.body.price){
+return res.status(404).json({
+    status: 'fail',
+    message: 'Missing name or price'
+    })
+}
+    next();
+}
 exports.getAllTours = (req,res)=>{
     res.status(200).json({
         status: 'success',
@@ -23,14 +42,8 @@ exports.getTour = (req,res)=>{
     //         message: 'Invalid id'
     //     });
     // }
-
-   const tour = tours.find(el=> el.id === id);
-   if(!tour){
-    return res.status(404).json({
-        status: 'failed',
-        message: 'Invalid Id'
-    });
-}
+    const tour = tours.find(el=> el.id === id);
+ 
     res.status(200).json({
         status: 'success',
         data: { 
@@ -54,14 +67,7 @@ exports.createTour = (req,res)=>{
     };
 
     exports.updateTour = (req,res)=>{
-        const id = req.params.id * 1;
-    
-        if(id > tours.length){
-            return res.status(404).json({
-                status: 'failed',
-                message: 'Invalid id'
-            });
-        }
+       
     res.status(200).json({
         status: 'success',
     data: {
@@ -71,14 +77,6 @@ exports.createTour = (req,res)=>{
     }
 
     exports.deleteTour = (req,res)=>{
-        const id = req.params.id * 1;
-    
-        if(id > tours.length){
-            return res.status(404).json({
-                status: 'failed',
-                message: 'Invalid id'
-            });
-        }
     res.status(204).json({
         status: 'success',
       data: null
