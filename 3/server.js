aceedb7163e51731cc5e6e474fb57683e2100b25
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2022-09-16 21:31:24 +0100

    started using the mongo DB

--- 3/server.js
+++ 3/server.js
@@ -1,9 +1,16 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({path: './config.env'})
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
+const DB = process.env.DB;
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('DB connection established'))
 // console.log(process.env);
  
 app.listen(port,()=>{
