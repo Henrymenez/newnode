cd2d84f8632cfaaab91da70350c55a5cadaeb79e
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2022-10-15 17:15:35 +0100

    setup global error handler

--- 3/app.js
+++ 3/app.js
@@ -3,33 +3,34 @@ const app = express();
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 
 // 1)middlewares
-if(process.env.NODE_ENV == 'development'){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-req.requestTime = new Date().toISOString();
-    next();
-})
-
-app.get('/', (req, res) => {
-    res.send({message: 'hello from app'});
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-app.use('/api/v1/tours',tourRouter);
-app.use('/api/v1/users',userRouter);
+app.get('/', (req, res) => {
+  res.send({ message: 'hello from app' });
+});
 
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-app.all('*',(req,res,next)=>{
-    res.status(404).json({
-        status: 'fail',
-        message: `Cant find ${req.originalURL} on this server`
-    })
-})
-module.exports = app; 
+app.all('*', (req, res, next) => {
+  next(new AppError(`Cant find ${req._parsedOriginalUrl.pathname} on this server`));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
