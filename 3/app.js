89ae7c52347e98f5e88bb48eafb579ebef0abc8f
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2022-10-17 22:32:42 +0100

    done with auth

--- 3/app.js
+++ 3/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-// const rateLimit = require('express-rate-limit');
-// const helmet = require('helmet')
-//const mongoSanitize = require('express-mongo-sanitize')
-//const xss = require('xss-clean');
-//const  hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean');
+const  hpp = require('hpp');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -13,20 +13,20 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 // 1)global middlewares
 //set security http headers
-// app.use(helmet())
+app.use(helmet())
 
 //development logging
 if (process.env.NODE_ENV == 'development') {
   app.use(morgan('dev'));
 }
 //limit request from same ip
-// const limiter = rateLimit({
-//   max: 100,
-//   windowMs: 60 * 60 * 1000,
-//   message: "Too many request from this IP, try again in an hour"
-// });
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP, try again in an hour"
+});
 
-// app.use('/api',limiter);
+app.use('/api',limiter);
 
 //body parser, reading data from body to req.body
 app.use(
@@ -36,21 +36,21 @@ app.use(
 );
 
 //data sanitization against NOsql query injection and XSS
-// app.use(mongoSanitize());
-// app.use(xss());
+app.use(mongoSanitize());
+app.use(xss());
 
 //prevent parameter polution
-// app.use(
-  //hpp({
-//   whitelist: [
-// 'duration',
-// 'ratingAverage',
-//'ratingsQauntity',
-//'maxGroupSize',
-//'difficulty',
-//'price'
-//   ]
-// }))
+app.use(
+  hpp({
+  whitelist: [
+'duration',
+'ratingAverage',
+'ratingsQauntity',
+'maxGroupSize',
+'difficulty',
+'price'
+  ]
+}))
 
 //serving static files
 app.use(express.static(`${__dirname}/public`));
