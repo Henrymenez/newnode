5ddb0461eb4663581be5b35d99a3545e698cbf7d
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2022-10-17 13:06:18 +0100

    set jwt to cookie

--- 3/controllers/authController.js
+++ 3/controllers/authController.js
@@ -12,16 +12,30 @@ const signToken = (id) => {
   });
 };
 
-exports.signup = catchAsync(async (req, res) => {
-  const newUser = await User.create(req.body);
-  const token = signToken(newUser._id);
-  res.status(201).json({
+const createSendToken = (user,statusCode,res)=>{
+  const token = signToken(user._id);
+const cookieOptions =  {
+  expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+  httpOnly: true
+};
+
+if(process.env.NODE_ENV === 'production') {
+  cookieOptions.secure = true
+}
+  res.cookie('jwt',token, cookieOptions);
+  user.password = undefined
+  res.status(statusCode).json({
     status: 'success',
     token,
     data: {
-      user: newUser,
+      user
     },
   });
+}
+
+exports.signup = catchAsync(async (req, res) => {
+  const newUser = await User.create(req.body);
+  createSendToken(newUser,201,res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -42,6 +56,7 @@ exports.login = catchAsync(async (req, res, next) => {
     status: 'success',
     token,
   });
+  
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
